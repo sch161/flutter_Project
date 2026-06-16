fe6169f2ced36b12fe08a7d9047450f898313584
민서연 <min@minseoyeon-ui-MacBookPro.local>
2026-06-16 21:51:37 +0900

    const/colors.dart 테마 파일 추가

---
+++ lib/const/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// 구글 매터리얼 디자인과 플러터 기본 도구 상자(버튼, 색상 등) 가져오기
+
+// 대표색 (앱의 시그니처, 하이라이트, 포인트 컬러)
+const primaryColor = Color(0xFF4A4A7E);
+// 배경색 (전체 화면의 바탕이 되는 색)
+const backgroundColor = Color(0xFFF7F6FF);
+// 박스 색 (TextField 입력창이나 카드 형태의 박스 내부 채움색)
+final boxColor = Colors.white;
+// 버튼 색 (일반 버튼이나 하단 바 버튼 등의 색상)
+const buttonColor = Color(0xFF4A4A7E);
